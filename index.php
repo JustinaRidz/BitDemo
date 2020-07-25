5a4e7a0fa8d49f7e830feef05fe974c05ebe082e
Justina <justina.ridz@gmail.com>
2020-07-25 17:27:32 +0300

    new demo

--- index.php
+++ index.php
@@ -1,26 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Document</title>
     <style>
         table {
-            border: 1px,  solid;
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td, th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #dddddd;
         }
     </style>
 </head>
+
 <body>
 
-<?php
- $failai = scandir('./');
-print_r($failai);
-foreach ($failai as $failas) {
-    print('<ul>');
-    print('<li>'.$failas.'</li>');
-    print('</ul>');
-}
-?>
+    <?php
+
+    $path = "./" . str_replace('./', '', $_GET['path'], $i);
+    $dirContents = scandir($path);
+    print($path);
+
+    print("<table>
+    <tr>
+         <th>Name</th>
+         <th>Type</th>
+    </tr>");
+    for ($i = 0; $i < count($dirContents); $i++) {
+        // if ($dirContents[$i] === ".." or $dirContents[$i] === ".")
+        // continue;
+        print("<tr><td>");
+        // print($path . '/' . $dirContents[$i]);
+        if (is_dir($dirContents[$i])) {
+            print("Dir</td>");
+            print("<td><a href='?path=" . $path . $dirContents[$i] . "'>" . $dirContents[$i] . "</a></td></tr>");
+        } else {
+            print("File</td>");
+            print("<td>" . $dirContents[$i] . "</td></tr>");
+        }
+    }
+    print("</table>");
+    ?>
 
 </body>
+
 </html>
